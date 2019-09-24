a83adef3984448882614933c238878538fa2ef41
Aron Polner <aron.polner@gmail.com>
2019-09-24 12:18:55 +0200

    Added the logic to the UI_Screen script. onScreenStart and onScreenClose events were added.

---
+++ Assets/Code/Scripts/UI_Screen.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UI_Screen : MonoBehaviour
+    {
+        #region Variables
+        [Header("Main Properties")]
+        public Selectable m_StartSelectable;
+
+        [Header("Screen Events")]
+        public UnityEvent onScreenStart = new UnityEvent();
+        public UnityEvent onScreenClose = new UnityEvent();
+
+        Animator animator;
+        #endregion
+
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+
+            if(m_StartSelectable)
+            {
+                EventSystem.current.SetSelectedGameObject(m_StartSelectable.gameObject);
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        public virtual void StartScreen()
+        {
+            // If we have listeners / objects waiting for this event to happen
+            if(onScreenStart != null)
+            {
+                onScreenStart.Invoke();
+            }
+
+            if(animator)
+            {
+                HandleAnimator("show");
+            }
+        }
+
+        public virtual void CloseScreen()
+        {
+            if(onScreenClose != null)
+            {
+                onScreenClose.Invoke();
+            }
+
+            HandleAnimator("hide");
+        }
+
+        void HandleAnimator(string aTrigger)
+        {
+            if (animator)
+            {
+                animator.SetTrigger(aTrigger);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Code/Scripts/UI_System.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UI
+{
+    public class UI_System : MonoBehaviour
+    {
+        #region Variables
+        [Header("System Events")]
+        public UnityEvent onSwitchedScreen = new UnityEvent();
+
+        private Component[] screens = new Component[0];
+        private UI_Screen previousScreen;
+        public UI_Screen PreviousScreen { get { return previousScreen; } }
+
+        private UI_Screen currentScreen;
+        public UI_Screen CurrentScreen { get { return currentScreen; } }
+        #endregion
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            screens = GetComponentsInChildren<UI_Screen>(true);
+        }
+        #endregion
+
+        #region Helper Methods
+        public void SwitchScreens(UI_Screen aScreen)
+        {
+            if(aScreen)
+            {
+                if(currentScreen)
+                {
+                    //currentScreen.Close();
+                    previousScreen = currentScreen;
+                }
+
+                currentScreen = aScreen;
+                currentScreen.gameObject.SetActive(true);
+                //currentScreen.StartScreen();
+
+                if(onSwitchedScreen != null)
+                {
+                    onSwitchedScreen.Invoke();
+                }
+            }
+        }
+
+        public void GoToPreviouseScreen()
+        {
+            if(previousScreen)
+            {
+                SwitchScreens(previousScreen);
+            }
+        }
+
+        public void LoadScene(int sceneIndex)
+        {
+            StartCoroutine(WaitToLoadScene(sceneIndex));
+        }
+
+        IEnumerator WaitToLoadScene(int SceneIndex)
+        {
+            yield return null;
+        }
+        #endregion
+    }
+}
