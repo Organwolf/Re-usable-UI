3633f450a3a9394235b27435e07523d16b4b3c6a
Aron Polner <aron.polner@gmail.com>
2019-09-24 15:51:24 +0200

    Editor script added -> increases reusability of the UI system!

---
+++ Assets/Todo_App/Code/Editor/UI_Menus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UI
+{
+    public class UI_Menus : MonoBehaviour
+    {
+        [MenuItem("UI Tools/Create UI Group")]
+        public static void CreateUIGroup()
+        {
+            //Debug.Log("Creating UI group");
+            GameObject uiGroup = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Todo_App/Prefabs/UI/UI_GRP.prefab");
+            if (uiGroup)
+            {
+                GameObject createdGroup = (GameObject)Instantiate(uiGroup);
+                createdGroup.name = "UI_GRP";
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("UI Tools Warning", "Cannot find UI Group Prefab!", "OK");
+            }
+        }
+    }
+}
