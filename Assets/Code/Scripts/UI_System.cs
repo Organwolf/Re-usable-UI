69bf01dddc147c8732f462619e4e8cb39bf24b38
Aron Polner <aron.polner@gmail.com>
2019-09-24 14:40:07 +0200

    modular UI system set up. All assets can be extended either through an override (animations). For code extend the UI_Screen script.

--- Assets/Code/Scripts/UI_System.cs
+++ Assets/Code/Scripts/UI_System.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class UI_System : MonoBehaviour
     {
         #region Variables
+        [Header("Main Properties")]
+        public UI_Screen m_StartScreen;
         [Header("System Events")]
         public UnityEvent onSwitchedScreen = new UnityEvent();
+
+        [Header("Fader Properties")]
+        public Image m_Fader;
+        public float m_FadeInDuration = 1f;
+        public float m_FadeOutDuration = 1f;
 
         private Component[] screens = new Component[0];
         private UI_Screen previousScreen;
@@ -24,6 +32,17 @@ namespace UI
         void Start()
         {
             screens = GetComponentsInChildren<UI_Screen>(true);
+
+            if(m_StartScreen)
+            {
+                SwitchScreens(m_StartScreen);
+            }
+
+            if(m_Fader)
+            {
+                m_Fader.gameObject.SetActive(true);
+            }
+            FadeIn();
         }
         #endregion
 
@@ -34,18 +53,34 @@ namespace UI
             {
                 if(currentScreen)
                 {
-                    //currentScreen.Close();
+                    currentScreen.CloseScreen();
                     previousScreen = currentScreen;
                 }
 
                 currentScreen = aScreen;
                 currentScreen.gameObject.SetActive(true);
-                //currentScreen.StartScreen();
+                currentScreen.StartScreen();
 
                 if(onSwitchedScreen != null)
                 {
                     onSwitchedScreen.Invoke();
                 }
+            }
+        }
+
+        public void FadeIn()
+        {
+            if(m_Fader)
+            {
+                m_Fader.CrossFadeAlpha(0f, m_FadeInDuration, false);
+            }
+        }
+
+        public void FadeOut()
+        {
+            if(m_Fader)
+            {
+                m_Fader.CrossFadeAlpha(0f, m_FadeOutDuration, false);
             }
         }
 
